bb754bb1bd701c10b4cd58d18dd0853b5348b57b
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-11-20 10:31:05 +0100

    COMMIT GITHUB.

---
+++ Boletin9-6/src/boletin9/pkg6/Boletin96.java
@@ -0,0 +1,18 @@
+
+package boletin9.pkg6;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Boletin96 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Metodo9_6 obxObterSoldo=new Metodo9_6();
+        obxObterSoldo.Soldo();
+    }
+    
+}
