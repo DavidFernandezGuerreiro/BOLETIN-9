9475ee8eb58588551b5c48cec3c9a24c9cd5af69
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-11-20 08:36:19 +0100

    COMMIT GITHUB.

---
+++ Boletin9-1/src/boletin9/pkg1/Boletin91.java
@@ -0,0 +1,22 @@
+
+package boletin9.pkg1;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+import java.util.Scanner;
+public class Boletin91 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //1. Calcula a cantidade de números negativos, positivos e, ceros  que hai nun grupo de 10 numeros . 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        Metodo9_1 obxCalcular=new Metodo9_1();
+        obxCalcular.calcular();
+    }
+    
+}
