a5581c5ac158ff18e8b6b962ea8da56b95b8632f
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-11-20 08:37:16 +0100

    COMMIT GITHUB.

---
+++ Boletin9-3/src/boletin9/pkg3/Boletin93.java
@@ -0,0 +1,18 @@
+
+package boletin9.pkg3;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Boletin93 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Metodo9_3 obxArea=new Metodo9_3();
+        obxArea.calcularRectangulo();
+    }
+    
+}
