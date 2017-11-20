daba07ffa7d6f7df81667358c1e42a18e8ecb41c
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-11-20 08:37:44 +0100

    COMMIT GITHUB.

---
+++ Boletin9-5/src/boletin9/pkg5/Boletin95.java
@@ -0,0 +1,24 @@
+
+package boletin9.pkg5;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Boletin95 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Metodo9_5 obxSerie1=new Metodo9_5();
+        obxSerie1.serie1();
+        
+        Metodo9_5 obxSerie2=new Metodo9_5();
+        obxSerie2.serie2();
+
+        Metodo9_5 obxSerie3=new Metodo9_5();
+        obxSerie3.serie3();        
+    }
+    
+}
