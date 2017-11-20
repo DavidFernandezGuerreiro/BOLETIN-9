1222a37a9c27f91bbe83f170286bb3191d6f1dd2
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-11-20 08:37:02 +0100

    COMMIT GITHUB.

---
+++ Boletin9-2/src/boletin9/pkg2/Boletin92.java
@@ -0,0 +1,20 @@
+
+package boletin9.pkg2;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Boletin92 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+//2- Realiza un programa que calcule a suma e o producto dos números comprendidos entre 10 e 90 .
+    public static void main(String[] args) {
+        Metodo9_2 obxCalcular=new Metodo9_2();
+        obxCalcular.calcularSumaProducto();
+    }
+    
+}
